3c4df37149e47d6d5404db9ae41b0ce259d2eb41
AlexAntipin <antipin-it@mail.ru>
2023-04-21 06:28:55 +0400

    Modification of the Feige-Fiat-Shamir authentication protocol

--- 4 задание(Схема аутентификации Фейге -Фиата -Шамира)/sheige-fiat-shamir.py
+++ 4 задание(Схема аутентификации Фейге -Фиата -Шамира)/sheige-fiat-shamir.py
@@ -85,13 +85,11 @@ def step1():
     f4 = open('n.txt','r')
     n = int(f4.read())
     r = randint(1, n-1)
-    #print('r= ', r)
     x = (r**2) % n
     f5 = open('x.txt', 'w')
     f5.write(str(x))
     f6 = open('r.txt', 'w')
     f6.write(str(r))
-    #print('x= ',x)
     return
 
 
@@ -101,7 +99,6 @@ def step2():
     for _ in range(k):
         q = randint(0,1)
         b.append(q)
-    #print('b=', b)
     f7 = open('b.txt','w')
     for i in range(k):
         f7.write(str(b[i]) + '\n')
@@ -129,8 +126,6 @@ def step3():
     y = y * int(r) % int(n)
     f12 = open('y.txt','w')
     f12.write(str(y))
-    #print('y= ', y)
-    #print('n= ',n)
     return
 
 
@@ -147,7 +142,6 @@ def step4():
     f16 = open('Open_key.txt', 'r')
     for line in f16:
         v.append(int(line))
-    #print(v)
     f17 = open('b.txt', 'r')
     for line in f17:
         b.append(int(line))
@@ -155,12 +149,11 @@ def step4():
     for i in range(k):
         z = z * v[i]**b[i]
     z = (y ** 2 * z) % n
-    #print('z= ', z)
     if z != x:
         return False
     else:
         return True
-    return
+
 
 
 def check():
@@ -189,23 +182,23 @@ def check():
 
 
 if __name__ == "__main__":
-    check()
-    # while True:
-    #     action = input(
-    #         "Введите 0 - для генерации ключей, 1 - для 1 шага протокола,"
-    #         " 2 - для 2 шага протокола, 3 - для 3 шага протокола," + "\n" + "4 - для 4 шага протокола, "
-    #         "5 - для выполнения всех шагов проткола, 6 - для выхода:" + "\n")
-    #     if action == "0":
-    #         gen_key()
-    #     elif action == "1":
-    #         step1()
-    #     elif action == "2":
-    #         step2()
-    #     elif action == "3":
-    #         step3()
-    #     elif action == "4":
-    #         step4()
-    #     elif action == "5":
-    #         check()
-    #     elif action == "6":
-    #         exit()
+    #check()
+    while True:
+        action = input(
+            "Введите 0 - для генерации ключей, 1 - для 1 шага протокола,"
+            " 2 - для 2 шага протокола, 3 - для 3 шага протокола," + "\n" + "4 - для 4 шага протокола, "
+            "5 - для выполнения всех шагов проткола, 6 - для выхода:" + "\n")
+        if action == "0":
+            gen_key()
+        elif action == "1":
+            step1()
+        elif action == "2":
+            step2()
+        elif action == "3":
+            step3()
+        elif action == "4":
+            step4()
+        elif action == "5":
+            check()
+        elif action == "6":
+            exit()
